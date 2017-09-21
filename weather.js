dca465892c5feb0e1f30bbf14263bc34270ee6d0
Gabriel Saruhashi <gabrielsaruhashi@Gabriels-MBP.wireless.yale.internal>
2017-09-21 15:33:47 -0400

    Working

--- weather.js
+++ weather.js
@@ -30,10 +30,8 @@ console.log(zipCode);
 
 
 // direct way 
-client.get("http://samples.openweathermap.org/data/2.5/weather?zip=94040,us&appid=b1b15e88fa797225412429c1c50c122a1", function (data, response) {
+client.get("http://samples.openweathermap.org/data/2.5/weather?zip=" + zipCode +",us&appid=b1b15e88fa797225412429c1c50c122a1", function (data, response) {
     // parsed response body as js object 
-    console.log(data);
-    // raw response 
-    console.log(response);
+    console.log(data.main.temp);
 });
 
