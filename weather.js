581e862faba37c8231cb8925c32107acb13eb20f
Jacob Bendicksen <jacob@jacobbendicksen.com>
2017-09-21 15:28:31 -0400

    little bit of express

--- weather.js
+++ weather.js
@@ -1,27 +1,35 @@
 var Client = require('node-rest-client').Client;
- 
+var express = require('express');
+var app = express();
+
 var client = new Client();
- 
+
 
  var args = {
     data: { test: "hello" },
     headers: { "Content-Type": "application/json" }
 };
 
-// direct way 
-client.get("http://samples.openweathermap.org/data/2.5/weather?zip=94040,us&appid=b1b15e88fa797225412429c1c50c122a1", function (data, response) {
-    // parsed response body as js object 
-    console.log(data);
-    // raw response 
-    console.log(response);
+// direct way
+// client.get("http://samples.openweathermap.org/data/2.5/weather?zip=94040,us&appid=b1b15e88fa797225412429c1c50c122a1", function (data, response) {
+//     // parsed response body as js object
+//     console.log(data);
+//     // raw response
+//     console.log(response);
+// });
+//
+// // registering remote methods
+// client.registerMethod("jsonMethod", "http://remote.site/rest/json/method", "GET");
+//
+// client.methods.jsonMethod(function (data, response) {
+//     // parsed response body as js object
+//     console.log(data);
+//     // raw response
+//     console.log(response);
+// });
+
+app.get('/', (req, res) => {
+    res.render('index.html');
 });
- 
-// registering remote methods 
-client.registerMethod("jsonMethod", "http://remote.site/rest/json/method", "GET");
- 
-client.methods.jsonMethod(function (data, response) {
-    // parsed response body as js object 
-    console.log(data);
-    // raw response 
-    console.log(response);
-});
+
+app.listen(process.env.PORT || 4000);
