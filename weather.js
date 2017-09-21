dbbdc1aae08917070dd2e0a62b6e373babd3b404
Gabriel Saruhashi <gabrielsaruhashi@Gabriels-MBP.wireless.yale.internal>
2017-09-21 15:44:45 -0400

    Working

--- weather.js
+++ weather.js
@@ -5,11 +5,11 @@ var app = express();
 var client = new Client();
 
 
-var zipCode = process.argv[0];
-console.log(zipCode);
+var zipCode = process.argv[2];
 
 
 // direct way
+
 // client.get("http://samples.openweathermap.org/data/2.5/weather?zip=94040,us&appid=b1b15e88fa797225412429c1c50c122a1", function (data, response) {
 //     // parsed response body as js object
 //     console.log(data);
@@ -30,8 +30,10 @@ console.log(zipCode);
 
 
 // direct way 
-client.get("http://samples.openweathermap.org/data/2.5/weather?zip=" + zipCode +",us&appid=b1b15e88fa797225412429c1c50c122a1", function (data, response) {
+client.get("http://api.openweathermap.org/data/2.5/weather?zip=" + zipCode.toString() + ",us&appid=8a65f7d4edb2867806356b3887f999fa", function (data, response) {
     // parsed response body as js object 
-    console.log(data.main.temp);
+    console.log("Temperature in " + data.name + " is " + Math.floor(data.main.temp * 9/5 - 459.67));
+
+
 });
 
